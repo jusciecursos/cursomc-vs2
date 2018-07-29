b61e7818f549200697c2a9ba772dce5ceae67e89
jusciecursos <jusciecursos@gmail.com>
2018-07-28 23:32:30 -0500

    Criando repository e service para Categoria

---
+++ src/main/java/com/juscie/cursomc/repositories/CategoriaRepository.java
@@ -0,0 +1,11 @@
+package com.juscie.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.juscie.cursomc.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+
+}
---
+++ src/main/java/com/juscie/cursomc/services/CategoriaService.java
@@ -0,0 +1,24 @@
+package com.juscie.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.juscie.cursomc.domain.Categoria;
+import com.juscie.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria buscar(Integer id) {
+		
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+
+	}
+
+}
